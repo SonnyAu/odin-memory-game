5f4b448877d5a44055065069407f3b19cffd7228
Sonny Au <au.sonny10@gmail.com>
2025-02-22 02:15:56 -0800

    Implement score keeping and card shuffling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [servantData, setServantData] = useState([]);
-  let clicked = [];
+  const [clicked, setClicked] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
   useEffect(() => {
     fetch("https://api.atlasacademy.io/export/NA/basic_servant.json")
       .then((response) => response.json())
@@ -12,7 +14,37 @@ function App() {
         setServantData(data.slice(0, 18));
       });
   }, []);
-  function clickServant() {}
+  useEffect(() => {
+    const shuffleServants = () => {
+      const copyData = [...servantData];
+
+      for (let i = copyData.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [copyData[i], copyData[j]] = [copyData[j], copyData[i]];
+      }
+
+      setServantData(copyData);
+    };
+
+    shuffleServants();
+  }, [clicked]);
+
+  function clickServant(id) {
+    if (!clicked.includes(id)) {
+      setClicked((prev) => [...prev, id]);
+      setScore((prev) => prev + 1);
+    } else {
+      setBestScore((prev) => Math.max(prev, score));
+      setScore(0);
+      fetch("https://api.atlasacademy.io/export/NA/basic_servant.json")
+        .then((response) => response.json())
+        .then((data) => {
+          setServantData(data.slice(0, 18));
+        });
+      setClicked([]);
+    }
+  }
+
   function renderCards() {
     return servantData.map((servant) => {
       return (
@@ -20,7 +52,7 @@ function App() {
           key={servant.id}
           img={servant.face}
           name={servant.name}
-          onClick={clickServant}
+          onClick={() => clickServant(servant.id)}
         />
       );
     });
@@ -28,6 +60,11 @@ function App() {
 
   return (
     <>
+      <div id="scores">
+        <div>Score: {score}</div>
+        <div>Best Score: {bestScore}</div>
+      </div>
+
       <div id="cards">{renderCards()}</div>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import "../styles/Card.css";
 export default function Card(props) {
   return (
     <>
-      <div className="card">
+      <div className="card" onClick={props.onClick}>
         <img src={props.img} alt={`Face picture of ${props.name}`} />
         <div className="servant">{props.name}</div>
       </div>
